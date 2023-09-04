8d33c00a100864b1e1527dbe305fed933d05cd28
godambevaibhav <godambevaibhav5@gmail.com>
2023-09-04 13:36:59 +0530

    binding module added

---
+++ src/app/binding/binding.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BindingRoutingModule } from './binding-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BindingRoutingModule
+  ]
+})
+export class BindingModule { }
